4195f9c51084984b70e5275a1a29ef0fca80ca17
Naveenkota55 <naveen.kota@dal.ca>
2019-02-15 11:29:14 -0800

    task2

--- task2.py
+++ task2.py
@@ -1,2 +1,9 @@
 file1=open('access.log','r')
+file2=open('get.log','a')
+file3=open('post.log','a')
+file4=open('put.log','a')
+file4=open('put.log','a')
+file4=open('delete.log','a')
 
+for lines in file1:
+	if GET
